369e4cfa55d62fa7eb88fc98bfc7daf5f5cf355f
Anas TULIMAT <anas.tulimat@outlook.fr>
2021-02-23 23:07:09 +0100

    feat: add header, footer and landing components structure

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+    return(
+        <footer>
+            <div className="footer-container">
+                <p>Projet réalisé par Anas TULIMAT - 2021</p>
+                <p>Les icônes Wolverine, Iron-man, batman sont prises sur iconfinder.com</p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
