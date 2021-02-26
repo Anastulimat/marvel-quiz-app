74e33d7fba6880f202a2ad42a1c016de998f2f93
Anas TULIMAT <anas.tulimat@outlook.fr>
2021-02-26 19:44:04 +0100

    feat: add auth methods for firebase class

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,4 +1,5 @@
 import app from 'firebase/app';
+import 'firebase/auth';
 
 //Web app's Firebase configuration
 const firebaseConfig = {
@@ -12,13 +13,37 @@ const firebaseConfig = {
 };
 
 class Firebase {
-
     /**
      * Firebase constructor
      */
     constructor() {
         app.initializeApp(firebaseConfig);
+        this.auth = app.auth();
     }
+
+    /**
+     * Registration
+     * @param email
+     * @param password
+     */
+    signupUser = (email, password) => {
+        this.auth.createUserWithEmailAndPassword(email, password);
+    }
+
+    /**
+     * Login
+     * @param email
+     * @param password
+     */
+    loginUser = (email, password) => {
+        this.auth.signInWithEmailAndPassword(email, password);
+    }
+
+    signOutUser = () => {
+        this.auth.signOut();
+    }
+
+
 
 }
 
