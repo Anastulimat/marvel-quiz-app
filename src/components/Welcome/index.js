4fef4bd77f339c573f07f97f72cb195204895f2b
Anas TULIMAT <anas.tulimat@outlook.fr>
2021-02-27 12:32:25 +0100

    feat: handle user session on welcome page

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -1,16 +1,40 @@
-import React from "react";
+import React, {useState, useContext, useEffect, Fragment} from "react";
+import {FirebaseContext} from  "../Firebase";
 import Logout from "../Logout";
 import Quiz from "../Quiz";
 
-const Welcome = () => {
-    return(
-        <div className="quiz-bg">
-            <div className="container">
-                <Logout/>
-                <Quiz/>
+const Welcome = (props) => {
+
+    const firebaseContext = useContext(FirebaseContext);
+
+    const[userSession, setUserSession] = useState(null);
+
+    useEffect(() => {
+        let listener = firebaseContext.auth.onAuthStateChanged(user => {
+            user ? setUserSession(user) : props.history.push('/');
+        })
+
+        return () => {
+            listener();
+        }
+    }, []);
+
+    return userSession === null ?
+        (
+            <Fragment>
+                <div className="loader"/>
+                <p className="loaderText">Loading ...</p>
+            </Fragment>
+        )
+        :
+        (
+            <div className="quiz-bg">
+                <div className="container">
+                    <Logout/>
+                    <Quiz/>
+                </div>
             </div>
-        </div>
-    )
+        );
 }
 
 export default Welcome;
