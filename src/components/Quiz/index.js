e79da48ea7189db70315675c26bf8d717f51adf0
Anas TULIMAT <anas.tulimat@outlook.fr>
2021-02-27 18:45:33 +0100

    feat(Quiz): add Levels, ProgressBar and structure for Quiz componenet

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import Levels from "../Levels";
+import ProgressBar from "../ProgressBar";
 
-const Quiz = (props) => {
+class Quiz extends React.Component {
 
-    console.log(props);
+    render() {
+        const { pseudo } = this.props.userData;
 
-    return(
-        <div>
-            <h2>Pseudo: {props.userData.pseudo}</h2>
-        </div>
-    );
+        return(
+            <div>
+                <Levels/>
+                <ProgressBar/>
+                <h2>Notre question Quiz</h2>
+                <p className="answerOptions">Question 1</p>
+                <p className="answerOptions">Question 2</p>
+                <p className="answerOptions">Question 3</p>
+                <p className="answerOptions">Question 4</p>
+                <button className="btnSubmit">Suivant</button>
+            </div>
+        )
+    }
+
 }
 
 export default Quiz;
